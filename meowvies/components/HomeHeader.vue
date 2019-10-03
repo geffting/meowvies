a9daf4798031eaefeef987eb96b961adf8eaec65
gustavo <gustavo.effting94@gmail.com>
2019-10-03 04:13:21 -0300

    improving layout of meowvies

---
+++ meowvies/components/HomeHeader.vue
@@ -0,0 +1,145 @@
+<template>
+  <b-container class="header" fluid>
+    <b-row>
+      <b-col cols="1">
+        <img src="cat.png" class="cat-img">
+      </b-col>
+      <b-col cols="2" />
+      </b-col>
+      <b-col cols="3" class="main-links">
+        <nuxt-link
+          to="/"
+        >
+          <b>Populares</b>
+        </nuxt-link>
+      </b-col>
+      <b-col v-if="loggedUser" cols="3" class="main-links">
+        <nuxt-link
+          to="/favorites"
+        >
+          <b>Seus Favoritos</b>
+        </nuxt-link>
+      </b-col>
+      <b-col v-else cols="3" class="main-links">
+        <p v-b-tooltip.hover title="Faça login para acessar seus favoritos">
+          <b>Seus Favoritos</b>
+        </p>
+      </b-col>
+      <b-col cols="3" class="login-btn">
+        <b-button
+          v-b-modal.modal-login
+          variant="light"
+        >
+          Entrar
+        </b-button>
+        <b-button
+          v-b-modal.modal-register
+          variant="light"
+        >
+          Cadastre-se
+        </b-button>
+      </b-col>
+    </b-row>
+    <b-modal id="modal-login" title="Entrar" @ok="login">
+      <b-row class="md-form">
+        <b-col cols="3">
+          <label for="username">Username: </label>
+        </b-col>
+        <b-col cols="9">
+          <b-form-input id="username" v-model="username" type="text" />
+        </b-col>
+      </b-row>
+      <b-row class="md-form">
+        <b-col cols="3">
+          <label for="password">Senha: </label>
+        </b-col>
+        <b-col cols="9">
+          <b-form-input id="password" v-model="password" type="password" />
+        </b-col>
+      </b-row>
+    </b-modal>
+    <b-modal id="modal-register" title="Cadastre-se" @ok="register">
+      <b-row class="md-form">
+        <b-col cols="3">
+          <label for="username">Username: </label>
+        </b-col>
+        <b-col cols="9">
+          <b-form-input id="username" v-model="username" type="text" />
+        </b-col>
+      </b-row>
+      <b-row class="md-form">
+        <b-col cols="3">
+          <label for="password">Senha: </label>
+        </b-col>
+        <b-col cols="9">
+          <b-form-input id="password" v-model="password" type="password" />
+        </b-col>
+      </b-row>
+    </b-modal>
+  </b-container>
+</template>
+<script>
+export default {
+  name: 'HomeHeader',
+
+  data () {
+    return {
+      username: '',
+      password: '',
+      loggedUser: false
+    }
+  },
+
+  methods: {
+    login () {
+      // do login
+    },
+    register () {
+      // do register
+    }
+  }
+}
+</script>
+<style scoped>
+.header {
+  height: 90px;
+  text-align: center;
+  background-color: #C60021;
+  box-shadow: 0px 4px 15px 0px rgba(32,33,36,0.28);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+}
+
+.cat-img {
+  height: 90px;
+  width: auto;
+}
+
+.main-links {
+  padding: 33px 0px 33px 0px;
+}
+
+.main-links a {
+  color: black;
+}
+
+.main-links p {
+  text-decoration: line-through;
+}
+
+.login-btn {
+  padding: 25px 20px 25px 0px;
+  text-align: right;
+}
+
+.md-form {
+  margin: 10px 0px 10px 0px;
+}
+
+.md-form label {
+  margin-top: 5px;
+}
+
+</style>
---
+++ meowvies/pages/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <b-container class="main-container" fluid>
+    <home-header />
+    <b-row class="interval-header" />
+    <b-row align-h="center" class="default-margin">
+      <b-col cols="11">
+        <b-row>
+          <b-col>
+            <label>Procure por um filme:</label>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-form-input v-model="search" type="text" />
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+    <b-row v-for="movie in movies" :key="movie.id" class="default-margin" align-h="center">
+      <b-col cols="11">
+        <b-card>
+          <b-media no-body>
+            <b-media-aside vertical-align="center">
+              <b-img blank blank-color="#ccc" width="180" height="256" alt="placeholder" />
+              <b-media-body class="ml-3">
+                <h5>
+                  {{ movie.title }}
+                </h5>
+                <p>
+                  {{ movie.description }}
+                </p>
+              </b-media-body>
+            </b-media-aside>
+          </b-media>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import HomeHeader from '../components/HomeHeader'
+
+export default {
+  components: {
+    HomeHeader
+  },
+
+  data () {
+    return {
+      search: '',
+      movies: []
+    }
+  },
+
+  mounted () {
+    const movieList = [
+      {
+        'id': 1,
+        'title': 'Harry Potter',
+        'description': 'filme de magia'
+      },
+      {
+        'id': 2,
+        'title': 'Duro de Matar',
+        'description': 'filme de tiro'
+      },
+      {
+        'id': 3,
+        'title': 'Velozes e Furiosos',
+        'description': 'filme de carro'
+      }
+    ]
+
+    this.movies = movieList
+
+    console.log(this.movies) // eslint-disable-line no-console
+  }
+}
+</script>
+<style>
+.main-container {
+  margin-bottom: 20px;
+}
+
+.interval-header {
+  padding-top: 90px;
+}
+
+.default-margin {
+  margin-top: 15px;
+}
+
+</style>
