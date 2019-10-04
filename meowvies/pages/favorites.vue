b40447bc86751738f2f2fa5fa7dd06a01ea33c43
gustavo <gustavo.effting94@gmail.com>
2019-10-04 01:02:56 -0300

    page /favorites created

---
+++ meowvies/pages/favorites.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container fluid>
+    <home-header />
+    <b-row align-h="center" class="default-margin-top">
+      <b-col cols="11">
+        <h3>Seus filmes favoritos</h3>
+      </b-col>
+    </b-row>
+    <movie-list />
+  </b-container>
+</template>
+<script>
+import HomeHeader from '../components/HomeHeader'
+import MovieList from '../components/MovieList'
+
+export default {
+  components: {
+    HomeHeader,
+    MovieList
+  }
+}
+</script>
+<style scoped>
+</style>
